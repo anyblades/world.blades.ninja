b64dd194a3c3c222a7eb2d8661c8247d3d23d4f7
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-05-20 16:55:15 -0600

    refactor: update base config dependency and implement dynamic site data merging

--- .11ty/eleventy.config.js
+++ .11ty/eleventy.config.js
@@ -1,6 +1,5 @@
-import baseConfig from "@anyblades/eleventy-blades/base-config";
+import baseConfig from "@anyblades/eleventy-blades-base";
 import { readFileSync } from "node:fs";
-import pkg from "./package.json" with { type: "json" };
 
 export default function (eleventyConfig) {
   baseConfig(eleventyConfig);
@@ -8,7 +7,6 @@ export default function (eleventyConfig) {
   eleventyConfig.setIncludesDirectory("./.11ty/_includes/");
   eleventyConfig.addPassthroughCopy({ "./.11ty/_public/": "." });
   eleventyConfig.addPassthroughCopy("../**/*.png");
-  eleventyConfig.addGlobalData("site", pkg.site);
 
   //TODO: move to 11ty-blades?
   for (const line of readFileSync("./.gitignore", "utf8").split("\n"))
